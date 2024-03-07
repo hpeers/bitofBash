b885f6fd2a5c029ba5814b9ca47ffa60e29de93a
nathanforester <nfeugene86@gmail.com>
2024-03-07 14:02:44 +0000

    first commit

---
+++ script.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+sudo apt update
+
+sudo apt install docker.io -y
+
+mkdir /home/ubuntu/docker
+
+touch /home/ubuntu/docker/Dockerfile
+
+cat <<ENDL > /home/ubuntu/docker/Dockerfile
+FROM python:3.8
+
+RUN apt update
+ENDL
